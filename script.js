7f8bdfdf06453d78b4877e8c9c8a6b00d5430c8b
LuisMtzGarcia <ing.luismtzgarcia@gmail.com>
2022-06-16 12:55:12 -0500

    Create sum and subtract functions
    These two functions receive 2 parameters and either adds them or subtracts
    them.

---
+++ script.js
@@ -0,0 +1,19 @@
+/**
+ * Receives two numbers and adds them.
+ * @param {number} a 
+ * @param {number} b 
+ * @returns 
+ */
+function sum(a, b) {
+    return a + b;
+}
+
+/**
+ * Receives two numbers and subtracts them.
+ * @param {number} a 
+ * @param {number} b 
+ * @returns 
+ */
+function subtract(a, b) {
+    return a - b;
+}
